8869d0a94d7b342ebc10be620f71619503655d27
Christopher Nance <ctnance0@gmail.com>
2022-02-26 13:51:30 -0500

    bg image and location text added

--- script/script.js
+++ script/script.js
@@ -1,10 +1,30 @@
-async function fetchBackgroundImg() {
+// Helper function to create and use elements with less code
+function createElement(tag, parent, className = "") {
+  if (!parent) {
+    console.log("parent is null!");
+    return;
+  }
+  let element = document.createElement(tag);
+  element.className = className;
+  parent.appendChild(element);
+  return element;
+}
+
+async function loadDashboard() {
+  let data = await fetchImgData();
+  if (data) {
+    console.log(data);
+    setBackgroundImg(data.urls.full);
+    setLocationTag(data.location.title);
+  }
+}
+
+async function fetchImgData() {
   let response = await fetch(
     "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
   );
   let data = await response.json();
-  setBackgroundImg(data.urls.full);
-  console.log(data);
+  return data;
 }
 
 function setBackgroundImg(url) {
@@ -12,4 +32,14 @@ function setBackgroundImg(url) {
   body.style.backgroundImage = `url(${url})`;
 }
 
-fetchBackgroundImg();
+function setLocationTag(locationText) {
+  if (!locationText) return; // do not set location tag if text is blank
+  let locationElem = createElement(
+    "p",
+    document.querySelector("body"),
+    "location-text"
+  );
+  locationElem.textContent = locationText;
+}
+
+loadDashboard();
